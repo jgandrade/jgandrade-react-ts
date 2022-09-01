496eaf58076b56e17c13f25279c8c93da5c9b64a
jgandrade1 <johnglenn.andrade@gmail.com>
2022-09-02 00:51:09 +0800

    Finish card tomorrow

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -2,7 +2,7 @@ import data from './data';
 import ProjectsCard from './ProjectsCard';
 
 function Projects() {
-    const cards = data.map((card, i) => <ProjectsCard key={i + "jgandradeKey"} {...card} />)
+    const cards = data.map((card, i) => <ProjectsCard key={i} {...card} />)
     return (
         <div className="projects">
             <h2>Projects</h2>
--- src/components/ProjectsCard.tsx
+++ src/components/ProjectsCard.tsx
@@ -1,15 +1,20 @@
+import { ReactElement, JSXElementConstructor, ReactFragment, ReactPortal, Key } from "react";
 
 function ProjectsCard(props: any) {
+    let tools = props.tools.map((e: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | ReactFragment | ReactPortal | null | undefined, i: Key | null | undefined) => <p key={i}>{e}</p>);
+
     return (
         <div className="project-card">
             <a href={props.link} target="_blank"><img src={props.img} /></a>
+            <p>Click image to redirect to project</p>
             <div>
-                <p>Click image to redirect to project</p>
                 <h3>{props.title}</h3>
                 <p>{props.description}</p>
+            </div>
+            <div className="tools">
                 <h4>Below are the tools I used:</h4>
-                <div className="tools">
-                    {props.tools}
+                <div>
+                    {tools}
                 </div>
             </div>
         </div>
